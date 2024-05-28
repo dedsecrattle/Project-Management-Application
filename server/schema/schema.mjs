f925b3cbd9ad94f4612ce74827092ba4014c9b2e
dedsecrattle <itsprabxxx@gmail.com>
2024-05-28 23:04:28 +0530

    Finished Backend

--- server/schema/schema.mjs
+++ server/schema/schema.mjs
@@ -14,7 +14,7 @@ const ProjectType = new GraphQLObjectType({
     client: {
       type: ClientType,
       resolve(parent, args) {
-        return clients.find((client) => client.id == parent.clientId)
+        return Client.findById(parent.clientId)
       },
     },
   }),
